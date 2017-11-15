74d386e5dc3c8f2bf47e954749e1f52f059b991b
amandagg <amandagomez897@hotmail.com>
2017-11-15 18:08:47 +0100

    amanda: login

---
+++ aulescrai/inici.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+<html lang="es">
+	<head>
+		<?php include("meta.html");?>
+		<!-- Titol -->
+		<title>Reserva d'Aules CRAI</title>
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="./css/stylelogin.css">
+		<!-- Icona --> 
+		<link rel="shortcut icon" href="img/favicon.jpg" type="image/x-icon" />
+	</head>
+		
+		<div class="login">
+		
+			<div class="titulo">
+				Benvinguts a l'espai de Reserva d'Aules del CRAI
+			</div>
+			<div class="logo">
+				<img src="./img/logo_urv.png" href="www.urv.cat" height="240" width="300">
+
+			</div>
+			<div class="datos">
+				<form action="" method="post" name="formulari">
+									
+					<div align="left">	<label for="IDToken1">Nom d'usuari:  </label></div>
+					<input type="text" name="IDToken1" >				
+					
+					<div align="left">	<label for="IDToken2">Clau d'accés:   </label></div>
+					<input type="password" name="IDToken2" >
+							
+					<div align="right"> <input type="submit" name="Submit" value="Acceptar" class="boton">&nbsp;&nbsp; <input name="Submit2" value="Cancel&middot;lar" type="reset" class="boton"> </div>
+					
+				</form>
+			</div>
+		</div>
+	
+	</body>
+</html>
